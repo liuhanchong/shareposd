d7fcdc125cba0817ad4b765ff58ba4d1dedab36e
liuhanchong <18256916415@163.com>
2016-12-02 14:15:37 +0800

    Update sae_utils.h

--- shareposd/core/sae_utils.h
+++ shareposd/core/sae_utils.h
@@ -14,6 +14,6 @@ sae_bool_t sae_utils_process_deamon();
 
 sae_uint32_t sae_utils_crc(sae_char_t *data, sae_size_t len);
 
-sae_void_t sae_res_process_exit(sae_int_t status);
+sae_void_t sae_utils_process_exit(sae_int_t status);
 
 #endif /* _SAE_UTILS_H_INCLUDED_ */
